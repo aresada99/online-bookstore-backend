7f105d6644e67415aab1a903e3a9b324d87f3211
aresada99 <ares.ada.dora@gmail.com>
2024-02-19 11:42:28 +0300

    added cors and api key authentication

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ import dotenv from 'dotenv';
 import bookRouter from './routes/bookRoute.js'
 import userRouter from './routes/userRoute.js'
 import authRouter from './routes/authRoute.js'
+import apiKeyMiddleware from './middlewares/apiKeyMiddleware.js';
+import cors from 'cors'
 dotenv.config();
 
+
+// MONGO DB CONNECTION
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log("Connected to database successfuly");
@@ -14,21 +18,39 @@ mongoose.connect(process.env.MONGODB_URI)
         console.log(err);
     });
 
+
+// EXPRESS INIT
 const app = express();
 app.use(express.json());
 
+// CORS SETTINGS
+const corsOrigins = [];
+for (let i = 1; process.env[`ORIGIN_${i}`]; i++) {
+    corsOrigins.push(process.env[`ORIGIN_${i}`]);
+}
+
+const corsOptions = {
+    origin: corsOrigins
+};
+app.use(cors(corsOptions));
+
+// API KEY PROTECTION
+app.use('/api', apiKeyMiddleware);
+
+// ROUTE MIDDLEWARES
+app.use('/api/books', bookRouter)
+app.use('/api/users', userRouter)
+app.use('/api/auth', authRouter)
+
+
+// PORT SETTINGS
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, ()=>{
-    console.log("Server is running on port "+PORT);
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
 })
 
-app.use('/api/books', bookRouter)
-app.use('/api/users',userRouter)
-app.use('/api/auth',authRouter)
 
-
-
-
+// next() ERROR HANDLING
 app.use((err, req, res, next) => {
     res.status(err.status || 500).json({
         success: false,
